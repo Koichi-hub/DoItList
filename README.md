55efe4e9802efcc570c7480ee218845f706f9f8e
Koichi-hub <koichi.hub@gmail.com>
2022-07-31 09:51:45 +0500

    Frontend start

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DoItList
 ToDo приложение с интересной механикой. React.js + Express.js
+
+## Frontend
+- React.js
+- Webpack
+- Babel
+- ESLint
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/plugin-transform-react-jsx',
+            ],
+          }
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
