c814dd833a36a91a664d047cfb0ba6b29eed8317
Koichi-hub <koichi.hub@gmail.com>
2022-08-02 00:57:04 +0500

    Prepared for the merger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const register = require('./register');
 const login = require('./login');
-const test = require('./test');
 
 const router = express.Router();
 
 const routes = [
   register,
   login,
-  test,
 ];
 
 router.use('/api', routes);
